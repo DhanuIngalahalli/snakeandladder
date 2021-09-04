1119f0d33257cd212988ed999e550ec967a1075e
sonupooja <ingalahallipooja@gmail.com>
2021-09-04 19:40:38 +0530

    snakeladder line position

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
 
-namespace snakeandladder
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("snake and ladder");
-        }
-    }
+
+
+	public static void Main()
+	{
+		int player = 0, position = 0, snakeposition = 0, loose, win, dice = 0;
+
+		//Player Position at O position
+		Console.WriteLine("Player position " + player);
+
+
+		while (player != 100)
+		{
+			//rollling the die to get the die value
+			Random random = new Random();
+			int rollDic = random.Next(1, 7);
+			dice = dice + 1;
+
+			//checking whether the player ladder, snake or no play
+			Random random2 = new Random();
+			int check = random2.Next(0, 3);
+
+			switch (check)
+			{
+				case 0:
+					//Console.WriteLine ("NO PLAY");
+					//player=100;
+					break;
+				case 1:
+					player = player + rollDic;
+					Console.WriteLine("position :" + player);
+					break;
+				case 2:
+					player = player - rollDic;
+					snakeposition = player - rollDic;
+					//Console.WriteLine ("Player got Snake and his position "+player);
+					break;
+
+			}
+
+
+			if (player == 100)
+			{
+				player = 100;
+				win = player;
+				Console.WriteLine();
+				Console.WriteLine("win score is " + win);
+			}
+			else if (player >= 101)
+
+			{
+				player = 100;
+				loose = position;
+				Console.WriteLine();
+				Console.WriteLine("loose score is: " + loose);
+			}
+
+			// Console.WriteLine ( "Player Loose the game ");
+		}
+		Console.WriteLine();
+		Console.WriteLine("Number of time dice " + dice);
+
+	}
 }
+
